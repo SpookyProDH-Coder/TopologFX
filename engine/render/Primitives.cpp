84601e1e0b86dab3ffbcc7bfca265a52ba0fdde6
SpookyProDH-Coder <davitonventurero@gmail.com>
2026-05-01 12:38:24 +0200

    Definition of planes and quotient spaces

--- engine/render/Primitives.cpp
+++ engine/render/Primitives.cpp
@@ -1,72 +1,104 @@
 #include <vector>
 
 #include "Primitives.h"
+#include "../../topology/TopologyPolicies.h"
 #include "VertexLayouts.h"
 
 using namespace std;
 
 bgfx::VertexLayout PosColorVertex::ms_layout;
 
-Mesh Primitives::CreateCube() 
+Mesh Primitives::MS_Plane(const MetricSpace& ms, float size) 
 {
     PosColorVertex::init();
-
-    static const PosColorVertex s_cubeVertices[] = {
-        {-1.0f,  1.0f,  1.0f, 0xff0000ff }, { 1.0f,  1.0f,  1.0f, 0xff00ff00 },
-        {-1.0f, -1.0f,  1.0f, 0xffff0000 }, { 1.0f, -1.0f,  1.0f, 0xff00ffff },
-        {-1.0f,  1.0f, -1.0f, 0xffff00ff }, { 1.0f,  1.0f, -1.0f, 0xffffff00 },
-        {-1.0f, -1.0f, -1.0f, 0xffffffff }, { 1.0f, -1.0f, -1.0f, 0xff000000 },
-    };
-
-    static const uint16_t s_cubeIndices[] = {
-        0, 1, 2, 1, 3, 2,
-        4, 6, 5, 5, 6, 7,
-        0, 2, 4, 4, 2, 6,
-        1, 5, 3, 5, 7, 3,
-        0, 4, 1, 4, 5, 1,
-        2, 3, 6, 6, 3, 7
-    };
-
-    Mesh mesh;
-    mesh.vbh = bgfx::createVertexBuffer(bgfx::makeRef(s_cubeVertices, sizeof(s_cubeVertices)), PosColorVertex::ms_layout);
-    mesh.ibh = bgfx::createIndexBuffer(bgfx::makeRef(s_cubeIndices, sizeof(s_cubeIndices)));
-    return mesh;
-}
-
-Mesh Primitives::CreateFromMetricSpace(const MetricSpace& ms, float epsilon) 
-{
-    PosColorVertex::init();
+    const unsigned WHITE = 0xffffffff;
     const auto& setX = ms.getSet();
-    unsigned i, j, numPoints = setX.count;
+    float half = size / 2.0f;
+    vector<PosColorVertex> vertex;
+    vector<unsigned> indexes;
+    float px, py, pz;
+    unsigned i, offset;
 
-    // 1. Crear Vértices (Átomos)
-    vector<PosColorVertex> vertices(numPoints);
-    for (i = 0; i < numPoints; i++) 
+    for (i = 0; i < setX.count; ++i) 
     {
-        vertices[i].x = setX.elements[i].x;
-        vertices[i].y = setX.elements[i].y;
-        vertices[i].z = setX.elements[i].z;
-        vertices[i].abgr = 0xffffff00;
+        px = setX.elements[i].x;
+        py = setX.elements[i].y;
+        pz = setX.elements[i].z;
+        offset = (unsigned)vertex.size();
+
+        vertex.push_back({px-half, py+half, pz, WHITE});
+        vertex.push_back({px+half, py+half, pz, WHITE});
+        vertex.push_back({px-half, py-half, pz, WHITE});
+        vertex.push_back({px+half, py-half, pz, WHITE});
+
+        indexes.push_back(offset + 0); indexes.push_back(offset + 1); indexes.push_back(offset + 2);
+        indexes.push_back(offset + 1); indexes.push_back(offset + 3); indexes.push_back(offset + 2);
     }
 
-    vector<unsigned> indices;
-    for (i = 0; i < numPoints; ++i) 
+    return Primitives::buildMesh(vertex, indexes);
+}
+
+Mesh Primitives::QT_Space(const MetricSpace& ms, unsigned resolution, const QuotientPolicy& policy, const Embedding<bx::Vec3>& embedding)
+{
+    ParametricGrid grid(resolution+1, resolution+1);
+    unsigned i, j, i0, i1, i2, i3;
+
+    const auto& setX = ms.getSet();
+    
+    for (i = 0; i < setX.count; i++)
     {
-        for (j = i + 1; j < numPoints; ++j) 
+        float u = (float)(i % resolution) / (float)(resolution - 1);
+        float v = (float)(i / resolution) / (float)(resolution - 1);
+
+        grid.uv_points.push_back({u, v});
+    }
+
+    for (i = 0; i < resolution-1; i++)
+    {
+        for (j = 0; j < resolution-1; j++)
         {
-            if (ms.distance(i, j) < epsilon) 
-            {
-                indices.push_back((uint16_t)i);
-                indices.push_back((uint16_t)j);
-            }
+            i0 = j * (resolution+1) + i;
+            i1 = j * (resolution+1) + i+1;
+            i2 = (j+1) * (resolution+1) + i;
+            i3 = (j+1) * (resolution+1) + i+1;
+
+            grid.indexes.push_back(i0);
+            grid.indexes.push_back(i1);
+            grid.indexes.push_back(i2);
+
+            grid.indexes.push_back(i1);
+            grid.indexes.push_back(i3);
+            grid.indexes.push_back(i2);
         }
     }
 
-    Mesh mesh;
-    mesh.vbh = bgfx::createVertexBuffer(bgfx::copy(vertices.data(), sizeof(PosColorVertex) * vertices.size()), PosColorVertex::ms_layout);
-    mesh.ibh = bgfx::createIndexBuffer(bgfx::copy(indices.data(), sizeof(unsigned) * indices.size()));
-    
-    cout << "[DEBUG] Created mesh with " << indices.size() << " indices." << endl;
+    policy.applyRule(grid);
+    vector<PosColorVertex> final_vertices(setX.count);
 
+    KleinParameters kp;
+    MorbiusParameters mp;
+    for (i = 0; i < setX.count; i++)
+    {
+        Vec2 uv = grid.uv_points[i];
+        bx::Vec3 pos3D = embedding.map(uv, kp, mp);
+
+        final_vertices[i].x = pos3D.x;
+        final_vertices[i].y = pos3D.y;
+        final_vertices[i].z = pos3D.z;
+
+        final_vertices[i].abgr = 0xFF8F09FF;
+    }
+    return Primitives::buildMesh(final_vertices, grid.indexes);
+}
+
+Mesh Primitives::buildMesh(const vector<PosColorVertex>& vertex, const vector<unsigned>& indexes)
+{
+    Mesh mesh;
+    mesh.vbh = bgfx::createVertexBuffer(
+        bgfx::copy(vertex.data(), sizeof(PosColorVertex) * vertex.size()), 
+        PosColorVertex::ms_layout);
+    mesh.ibh = bgfx::createIndexBuffer(
+        bgfx::copy(indexes.data(), sizeof(unsigned) * indexes.size()),
+         BGFX_BUFFER_INDEX32);
     return mesh;
 }
