55eb54759a1c4e73d54ba75bc9a06bc229b82c34
SpookyProDH-Coder <davitonventurero@gmail.com>
2026-05-01 12:38:12 +0200

    Definition of planes and quotient spaces

--- engine/render/Primitives.h
+++ engine/render/Primitives.h
@@ -1,10 +1,21 @@
-#pragma once
+#ifndef _H_PRIMITIVES
+#define _H_PRIMITIVES
+
+#include <vector>
 #include "VertexLayouts.h"
 #include "../../topology/MetricSpace.h"
+#include "../../topology/TopologyPolicies.h"
 #include "../core/Registry.h"
 
 namespace Primitives 
 {
-    Mesh CreateCube();
-    Mesh CreateFromMetricSpace(const MetricSpace&, float);
-}
+    /* Mesh builders */
+    Mesh MS_Plane(const MetricSpace&, float);
+
+    Mesh QT_Space(const MetricSpace&, unsigned, const QuotientPolicy&, const Embedding<bx::Vec3>&);
+
+    /* Auxiliars */
+    Mesh buildMesh(const vector<PosColorVertex>&, const vector<unsigned>&);
+};
+
+#endif
