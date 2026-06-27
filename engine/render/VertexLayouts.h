b8ff725ab8e14e229c38444b84074ce377b7009a
SpookyProDH-Coder <davitonventurero@gmail.com>
2026-06-27 18:42:46 +0200

    Commenting.

--- engine/render/VertexLayouts.h
+++ engine/render/VertexLayouts.h
@@ -1,14 +1,35 @@
+/******* VertexLayouts.h ***************************************************/ /**
+ *
+ * @file VertexLayouts.h
+ *
+ * Vertex memory layout used in the rendering pipeline
+ *  
+ * @version 1.0
+ * @author SpookyProDH-Coder
+ * @date 14/06/2026
+ ***************************************************************************/
+
 #ifndef _H_VERTEX_LAYOUTS
 #define _H_VERTEX_LAYOUTS
+
 #include <bgfx/bgfx.h>
 
+/**
+ * @brief Vertex format:
+ * - Position (X,Y,Z): 3 * sizeof(float) = 12 bytes
+ * - Color (AGBR):     1 * sizeof(uint32_t) = 4 bytes
+ */
 struct PosColorVertex 
 {
-    float x, y, z;
-    unsigned abgr;
+    float x, y, z;  ///< 3D corrdinates (X, Y, Z)
+    unsigned abgr;  ///< 32-bit ABGR color (0xAABBGGRR)
 
-    static bgfx::VertexLayout ms_layout;
+    static bgfx::VertexLayout ms_layout;    ///< Shared GPU vertex layout specification
 
+    /** Function init
+     * 
+     * @brief Initialize static vertex layout registry
+     */
     static void init()
     {
         ms_layout.begin()
